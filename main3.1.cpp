a6f21ee573fb56f4bfc4eec595b260d92d97352c
Kalinin <ivankallin@mail.ru>
2022-03-09 19:02:46 +0300

    3.3 Замена умножения на деление

--- main3.1.cpp
+++ main3.1.cpp
@@ -7,5 +7,5 @@ int main()
 	cin >> a >> b;
 	cout << "A + B = " << a + b << '\n'
 	cout << "A - B = " << a - b << '\n'
-	cout << "A * B = " << a * b << '\n';
+	cout << "A / B = " << a * b << '\n';
 }
